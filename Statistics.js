9f34a3c06f676bad6f65c488355034d2f5c26842
Michal <michalcira@gmail.com>
2022-11-15 17:11:42 +0100

    Add class Statistics

--- Statistics.js
+++ Statistics.js
@@ -1,5 +1,5 @@
 class Statistics {
-    constructor() {
+    constructor(win, bid) {
         this.gameResult = [{
             win: true,
             bid: 2
@@ -8,6 +8,21 @@ class Statistics {
             bid: -12
         }];
 
+    }
+
+    addGameToStats(win, bid) {
+        let gameResult = {
+            win,
+            bid
+        }
+        this.gameResult.push(gameResult)
+    }
+
+    showGameToStats() {
+        let games = this.gameResult.length;
+        let wins = this.gameResult.filter(result => result.win === true).length;
+        let loss = this.gameResult.filter(result => result.win === !true).length;
+        return [games, wins, loss]
 
     }
 }
