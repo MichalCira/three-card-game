9dd298df955c2ac08c548d84e47c61497bac3873
Michal <michalcira@gmail.com>
2022-11-15 15:53:47 +0100

    creating two methods in the Wallet class

--- Wallet.js
+++ Wallet.js
@@ -1,8 +1,23 @@
-class Wallet{
+class Wallet {
     constructor(money) {
-        
-        const _money = money;
-        this.getMoney() = () => _money;
+
+        let _money = money;
+        this.getMoney = () => _money;
+
+        this.changeMoney = (value, type = "+") => {
+            if (value < 1) {
+                throw new Error("za mała wartość")
+            } else if (typeof (value) === 'number' && !isNaN(value)) {
+                if (type === "+") {
+                    return _money += value
+                } else {
+                    return _money -= value
+                }
+            }
+        }
 
     }
+
 }
+
+const wallet = new Wallet(200)
